325b610e5581285ffceeba33bb46384d91b39f57
Aram Joseph <ajoseph@utc4me.org>
2017-03-09 10:28:31 -0500

    Woo sounds

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour
+{
+    float waitBeforeClip;
+    [SerializeField]
+    public bool play = false;
+
+    public List<AudioClip> audioClips;
+
+    AudioSource AS;
+    private float initialValue;
+
+    // Use this for initialization
+    void Awake()
+    {
+        initialValue = waitBeforeClip;
+        AS = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    PlaySound();
+        //}
+        waitBeforeClip -= Time.deltaTime;
+        if (waitBeforeClip <= 0f && play)
+        {
+            PlaySound();
+            waitBeforeClip = AS.clip.length;
+        }
+    }
+
+    public void PlaySound()
+    {
+        int clipNumber = Mathf.RoundToInt(Random.Range(0f, audioClips.Count));
+        AS.Stop();
+        AS.clip = audioClips[clipNumber];
+        AS.Play();
+        play = false;
+    }
+
+}
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,18 +11,23 @@ public class Player : MonoBehaviour {
 
     Rigidbody rbody;
     private bool onGround;
+    private bool firstTime = true;
     private float jumpLock;
     private int spacePressed;
     private DisplayManager displayManager;
+    private AudioManager audioManager;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         rbody = GetComponent<Rigidbody>();
         displayManager = GameObject.FindObjectOfType<DisplayManager>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        audioManager = GameObject.FindObjectOfType<AudioManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         Jump();
 		
@@ -33,6 +38,7 @@ public class Player : MonoBehaviour {
         if (Input.GetButtonDown("Jump") && jumpLock <= 0)
         {
             rbody.AddForce(Vector3.up * jumpForce);
+            audioManager.PlaySound();
             jumpLock = jumpWait;
             spacePressed++;
         }
@@ -52,16 +58,21 @@ public class Player : MonoBehaviour {
         if (spacePressed > 0 && onGround)
         {
             displayManager.mainText.text = "Good job!";
-            Debug.Log("Winning!");
+            //Debug.Log("Winning!");
         }
     }
 
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log("joe");
+        //Debug.Log("joe");
         if (other.gameObject.tag == "Ground")
         {
             onGround = true;
+            if (!firstTime)
+            {
+                audioManager.PlaySound();
+            }
+            firstTime = false;
             CheckObjective();
         }
     }
